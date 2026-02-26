d693b4cf233f8da79afb1a9db009bffdb77dfae2
Nataly 07 <nataballesterossierra@gmail.com>
2026-02-26 17:42:38 -0500

    codigo no terminado

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int opc = 0 ;
+        Mascota m1 = new Mascota();
+
+        System.out.println("   Sistema de la Veterinaria   ");
+        System.out.println("");
+        System.out.println("Para registrar a tu mascota por fa escoge alguna de las opciones: ");
+        System.out.println("");
+
+
+        opc = teclado.nextInt();
+        teclado.nextLine();
+        while (true){
+            System.out.println("1. Registrar Nombre de tu mascota ");
+            System.out.println("2. Mostrar informacion sobre el peso de tu mascota");
+            System.out.println("3. Mostrar la ficha Medica de tu mascota ");
+            System.out.println("4. Modificar el peso y la edad de tu mascota");
+            System.out.println("5. Salir");
+            opc = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opc){
+
+                case 1:
+                    System.out.println(" Ingresa el nombre de tu mascota: ");
+                    String nombre = teclado.nextLine();
+                    System.out.println("Ingrese el tipo de Especie de su mascota: ");
+                    String especie = teclado.nextLine();
+                    System.out.println("Ingrese la edad: ");
+                    int edad = teclado.nextInt();
+                    System.out.println(" Ingrese el peso de su mascota: ");
+                    double peso = teclado.nextDouble();
+                    break;
+                case 2:
+                    System.out.println("");
+
+                    break;
+                case 3:
+                    m1.mostrarFicha();
+
+                    break;
+                case 4:
+                    System.out.println("modificar el peso ");
+
+
+                    break;
+                case 5:
+                    System.out.println("");
+                    break;
+
+                default:
+                    System.out.println("No existe");
+
+            }
+        }
+
+
+
+    }
+}
---
+++ src/Mascota.java
@@ -0,0 +1,110 @@
+public class Mascota {
+
+    private String nombre;
+    private String especie;
+    private int edad;
+    private double peso;
+    private boolean saludable;
+
+    public Mascota() {
+    }
+
+    public Mascota(String nombre, String especie, int edad, double peso, boolean saludable) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.peso = peso;
+        this.saludable = saludable;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public boolean isSaludable() {
+        return saludable;
+    }
+
+    public void setSaludable(boolean saludable) {
+        this.saludable = saludable;
+    }
+
+    @Override
+    public String toString() {
+        return "Mascota{" +
+                "nombre='" + nombre + '\'' +
+                ", especie='" + especie + '\'' +
+                ", edad=" + edad +
+                ", peso=" + peso +
+                ", saludable=" + saludable +
+                '}';
+    }
+
+    // Metodos
+
+    public int cumplirAños (int edad){
+        System.out.println(" Ingresa la edad que va a cumplir tu mascota  ");
+        return edad;
+    }
+
+    public int  engordar(){
+
+        int  engordar = 17;
+
+        if (peso < 15 ){
+            System.out.println("La mascota esta muy baja de peso");
+            System.out.println("Debes seguir las indicaciones para que suba de peso por su salud");
+        }
+        return engordar;
+
+    }
+
+    public int  adelgazar(){
+         int adelgazar = 56;
+         if (peso > adelgazar){
+             System.out.println("Tu mascota tiene sobre peso");
+         }
+         return adelgazar;
+
+    }
+
+    public void  recuperarSalud(){
+
+    }
+
+    public String mostrarFicha(){
+        System.out.println("Nombre: "+ this.getNombre());
+        System.out.println("Especie: "+ this.getEspecie());
+        System.out.println("Edad: "+ this.getEdad());
+        System.out.println("Peso: "+this.getPeso());
+        return "";
+    }
+}
